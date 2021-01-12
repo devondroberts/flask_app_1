48df875b9fb1ae5d9ad5b700d610eabce46ffd13
Devon Roberts <devondroberts@gmail.com>
2021-01-12 01:58:42 +0000

    Yay, it's all finished!

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template, redirect, render_template, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -26,13 +26,13 @@ class Comment(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     content=db.Column(db.String(4096))
 
-comments = []
-
-@app.route('/', methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+        return render_template("main_page.html", comments=Comment.query.all())
 
-    comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
     return redirect(url_for('index'))
 
